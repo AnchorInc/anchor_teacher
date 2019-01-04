6487967870a3b08dad48e7a0c24920c201123998
spyking <raghv.indr@gmail.com>
2019-01-05 03:33:06 +0530

    fix display name

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { AppRegistry, Platform } from 'react-native';
 import App from './app/index';
+import { name as appName } from './app.json';
 import { backgroundMessageListener, backgroundActionHandler } from './app/cloudmessaging';
 
-AppRegistry.registerComponent('anchor', () => App);
+AppRegistry.registerComponent(appName, () => App);
 if (Platform.OS === 'android') {
   AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => backgroundMessageListener);
   AppRegistry.registerHeadlessTask('RNFirebaseBackgroundNotificationAction', () => backgroundActionHandler);
