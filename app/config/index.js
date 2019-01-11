423e838e6015bd37f02e710d556fdf167e01d92c
Nithin Srinivasan <nithinsrinivasan@outlook.com>
2019-01-11 22:10:48 +0530

    fixed dumb mistake

--- app/config/index.js
+++ app/config/index.js
@@ -1,13 +1,6 @@
 export * from './styles';
 export * from './settings';
 export * from './store';
-<<<<<<< HEAD
+
 export * from './notifications';
-=======
-<<<<<<< HEAD
-// export * from './notifications';
-=======
-export * from './notifications';
->>>>>>> c8ae2d4bca23f0d82a73b2e5bbce8971ba671aae
->>>>>>> setup
 export { default } from './googleSignin';
